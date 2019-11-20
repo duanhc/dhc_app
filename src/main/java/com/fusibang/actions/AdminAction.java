e954f4ee9a0c80bb4347781918a968b9039df287
duanhc <505343563@qq.com>
2019-11-20 20:22:24 +0800

    Test：sourceTree回滚测试

--- src/main/java/com/fusibang/actions/AdminAction.java
+++ src/main/java/com/fusibang/actions/AdminAction.java
@@ -5,19 +5,17 @@
 
 package com.fusibang.actions;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.struts2.interceptor.ServletRequestAware;
-import org.apache.struts2.interceptor.ServletResponseAware;
-
 import com.fusibang.services.AdminService;
 import com.fusibang.tables.Admin;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class AdminAction extends ActionSupport implements ServletRequestAware, ServletResponseAware, ModelDriven<Admin> {
     private static final long serialVersionUID = 1L;
@@ -33,7 +31,7 @@ public class AdminAction extends ActionSupport implements ServletRequestAware, S
     public String manage() {
         return this.adminService.manage(this.request);
     }
-
+    //test
     public String login() {
         HttpSession session = this.request.getSession();
         String result;
